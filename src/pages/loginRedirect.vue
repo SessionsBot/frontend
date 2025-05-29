7f980f13929142e325effd0f1025c3ca1dab04ea
br4dyb <halting.credo.00@icloud.com>
2025-05-29 03:55:29 -0500

    ~

--- src/pages/loginRedirect.vue
+++ src/pages/loginRedirect.vue
@@ -1,6 +1,6 @@
 <script setup>
 // App Imports:
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 const route = useRoute()
@@ -10,22 +10,41 @@ const auth = useAuthStore()
 // Component Imports:
 import siteHeader from '../components/flex/siteHeader.vue'
 
+// Element Variables:
+const statusMessage = ref('{ ! }')
+const titleSubHeading = ref('Loading')
+
 // On Page Load Event:
 onMounted(() => {
     // Get provided login token:
     const token = route.query.token
-    const redirect = true;
+    const encodedUser = route.query.user
+    const redirect = false;
     const statusFooterText = document.getElementById('statusFooterText')
+    
+    let user;
+    if(encodedUser) {
+        // User Data Received:
+        try {
+            const decoded = atob(decodeURIComponent(encodedUser));
+            user = JSON.parse(decoded);
+        } catch (e) {
+            console.error('Failed to decode user:', e);
+        }
+    }
 
-    if (token) {
+    if(user) {
         // Token provided:
-        statusFooterText.innerText = `token: ${token}`
-        auth.loginWithToken(token)
+        statusMessage.value = `User Id: ${user.id}`
+        auth.loginWithToken(user.id)
+        const stringUserData = JSON.stringify(user)
+        alert(`User Found:
+        ${stringUserData}`)
         if(redirect) {router.push('/dashboard')}
 
     } else {
         // Token NOT provided:
-        statusFooterText.innerText = `Login Failed!`
+        statusMessage.value = `Login Failed!`
         statusFooterText.classList.add('text-rose-600', 'font-black')
         if(redirect) {setTimeout(()=>{router.push('/')},3_000)}
     }
@@ -48,7 +67,7 @@ const homepage = () => router.push('/')
         <div class="rounded-md bg-black/75 text-white p-4 text-center flex flex-col gap-3 justify-center items-center">
 
             <p class="opacity-75 font-black text-xl py-1"> 
-                ⏳ PLEASE WAIT 
+                ⏳ PLEASE WAIT | {{ titleSubHeading }}
             </p>
 
             <div class="w-[95%] h-[2px] rounded-md animate-gradient-x bg-gradient-to-r from-purple-500 via-blue-500 to-pink-500">
@@ -74,7 +93,7 @@ const homepage = () => router.push('/')
             <footer class="px-2.5 opacity-35 relative flex gap-1 flex-wrap justify-between items-center w-full h-fit flex-col">
             
                 <p id="statusFooterText" class="h-fit text-left text-[10px] italic"> 
-                    [ ! ] 
+                    {{ statusMessage }}
                 </p>
 
                 <svg id="loadingSpinIcon" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M14.07 5.32C16.26 6 18 7.74 18.68 9.93c.19.63.76 1.07 1.41 1.07h.04c1 0 1.72-.96 1.43-1.91-.97-3.18-3.48-5.69-6.66-6.66-.94-.29-1.9.43-1.9 1.43v.04c0 .66.44 1.23 1.07 1.42zm4.61 8.75c-.68 2.2-2.42 3.93-4.61 4.61-.63.19-1.07.76-1.07 1.41v.04c0 1 .96 1.72 1.91 1.43 3.18-.97 5.69-3.48 6.66-6.66.29-.95-.43-1.91-1.42-1.91h-.05c-.66.01-1.23.45-1.42 1.08zM11 20.11c0-.67-.45-1.24-1.09-1.44C7.07 17.78 5 15.13 5 12s2.07-5.78 4.91-6.67c.64-.2 1.09-.77 1.09-1.44v-.01c0-1-.97-1.74-1.93-1.44C4.98 3.69 2 7.5 2 12c0 4.5 2.98 8.31 7.07 9.56.96.3 1.93-.44 1.93-1.45z"/></svg>
