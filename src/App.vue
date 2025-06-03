22d7b1ff252d583e2e37a636a88a177afe9cb9c8
br4dyb <halting.credo.00@icloud.com>
2025-06-02 21:12:33 -0500

    ~

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <script setup>
-  // Imports:
-  import { ref } from 'vue';
+// Imports:
+import { ref } from 'vue';
 
-  // Components:
-  import siteHeader from './components/siteHeader.vue';
-  import siteFooter from './components/siteFooter.vue';
+// Components:
+import siteHeader from './components/siteHeader.vue';
+import siteFooter from './components/siteFooter.vue';
 
-  // Vairables:
-  const siteVersion = ref("0.2.8b")
+// Vairables:
+const siteVersion = ref("0.2.8b")
 </script>
 
 
 <template>
 
+  
   <siteHeader />
   <!-- ↓ Header Padding ↓ -->
   <div class="h-12 sm:h-14 w-full"></div>
 
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="$route.fullPath" />
+    </transition>
+  </router-view>
 
-  <siteFooter :siteVersion="siteVersion" />
+  
+  <Transition name="fade" mode="out-in">
+    <siteFooter :siteVersion="siteVersion"></siteFooter>
+  </Transition>
 
 </template>
-
