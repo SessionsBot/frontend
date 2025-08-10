b1c5bbeaca4809271d1c09ffd30cc79958c0ef21
br4dyb <halting.credo.00@icloud.com>
2025-08-10 06:30:05 -0500

    fix: Route to new API endpoints

--- src/utils/modules/backendApi.ts
+++ src/utils/modules/backendApi.ts
@@ -12,7 +12,7 @@ import { GuildDataResponse, SecureActionResponse, SecureActionNames } from "@ses
  * @param guildId Discord Guild ID to fetch data for
  */
 export async function getGuildData(guildId) : Promise <GuildDataResponse> {
-    const requestUrl = 'https://brilliant-austina-sessions-bot-discord-5fa4fab2.koyeb.app/api/discord/guild?guildId=' + guildId;
+    const requestUrl = 'https://brilliant-austina-sessions-bot-discord-5fa4fab2.koyeb.app/api/api/v2/guilds/' + guildId;
 
     let responseData : GuildDataResponse;
 
@@ -35,68 +35,4 @@ export async function getGuildData(guildId) : Promise <GuildDataResponse> {
         };
         return responseData;
     }
-}
-
-
-/** Attempts to execute a **secure action** within the backend API.
- *
- * @details 
- * - Requires Authentication (via token)
- * - *Specified Secure Actions Available*
- *      - Otherwise, make a direct API call...
- * 
- * @param authToken Authentication token to provide for secure action attempt.
- * 
- * @param actionType The secure action to attempt to execute.
- * 
- * @param guildId The Discord guild by ID to perform the secure action.
- * 
- * @param requestData Any further data to provide for the request, Please view the [backend API code](https://github.com/SessionsBot/backend/blob/main/src/webService) for required data for each action type.
- */
-export async function attemptSecureAction(authToken:string, actionType:SecureActionNames, guildId?:string, requestData?:any) : Promise <SecureActionResponse> {
-    
-    let responseData : SecureActionResponse;
-
-    try {
-        // 1. Attempt Secure Action:
-        const requestUrl = `https://brilliant-austina-sessions-bot-discord-5fa4fab2.koyeb.app/api/secure-action`;
-        const response = await fetch(requestUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${authToken}`
-            },
-            body: JSON.stringify({
-                actionType: 'SETUP-GUILD',
-                guildId: guildId || 'null',
-                data: requestData
-            })
-        })
-
-        // 2. Get Request Response:
-        responseData = await response.json()
-
-        // 3. Check for Errors:
-        if(!responseData?.success) {
-            // Error Occurred:
-            throw responseData?.error?.message
-        }
-            
-        // 4. Success:
-        return responseData
-        
-    
-    } catch (error) {
-        // Error Occurred:
-        console.warn('API ERROR', 'Secure Action', error);
-        // Return Error:
-        return responseData = {
-            success: false,
-            error: {
-                code: 500,
-                message: error
-            }
-        }
-    }
-
 }
