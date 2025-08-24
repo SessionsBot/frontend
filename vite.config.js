ffdc214b7b4f77633c6bfde04b0a8489afe6a149
br4dyb <halting.credo.00@icloud.com>
2025-08-24 03:10:27 -0500

    fix: ignore type pkg on dev build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/frontend/', // - Used for GitHub Static Pages
+  // Plugins:
   plugins: [
     vue(),
     Components({
@@ -29,12 +29,18 @@ export default defineConfig({
       dts: './private/auto-imports.d.ts',   // optional, good for TS/IDE help
     }),
   ],
+  // Global Vars:
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
+  // Path Resolver:
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // Optimize/Ignore Deps:
+  optimizeDeps: {
+    exclude: ['@sessionsbot/api-types'] // don’t pre-bundle this package
+  }
 })
